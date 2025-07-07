28bfba2ce0a097bfb99899f760f3e0420624c88f
FebryanAkt <febryan08022006@gmail.com>
2025-07-07 07:10:16 +0700

    function

---
+++ pertemuan4/date.php
@@ -0,0 +1,15 @@
+<?php
+    // Date
+    // echo date("l, d-M-Y");
+
+    // // Time
+    // echo time ();
+    // echo date("l, d-M-Y", time()+60*60*24*1000);
+
+    // mktime
+    // jam, menit, detik, bulan, tanggal, tahun
+    // echo date ("l, d M Y", mktime(0,0,0,2,8,2006));
+
+    // strtotime
+    echo date ("l, d M Y", strtotime("8 feb 2006"));
+?>
